03814c74a0e4887e99554fc1108efe64a8001bc6
AlexTheMaggot <alexthemaggot23@gmail.com>
2024-09-26 15:45:19 +0500

    - add user model

---
+++ lelit_loyalty/api/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class User(models.Model):
+    user_id = models.IntegerField()
+    lang = models.CharField(max_length=2, null=True, blank=True)
+    name = models.CharField(max_length=50, null=True, blank=True)
+    phone = models.CharField(max_length=20, null=True, blank=True)
+    barcode = models.CharField(max_length=9, null=True, blank=True)
+    birth_date = models.CharField(max_length=10, null=True, blank=True)
+    city = models.CharField(max_length=20, null=True, blank=True)
+    balance = models.IntegerField()
