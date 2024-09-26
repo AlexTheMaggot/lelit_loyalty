460ba5e16826847478d04b8cb5f86cdbef2446e5
AlexTheMaggot <alexthemaggot23@gmail.com>
2024-09-26 15:43:26 +0500

    - create project

---
+++ lelit_loyalty/lelit_loyalty/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for lelit_loyalty project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lelit_loyalty.settings')
+
+application = get_asgi_application()
