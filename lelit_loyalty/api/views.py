9fd45551e03bc37709369349648ad39b582d4009
AlexTheMaggot <alexthemaggot23@gmail.com>
2024-10-16 23:22:47 +0500

    - add new purchase api method, add errors

--- lelit_loyalty/api/views.py
+++ lelit_loyalty/api/views.py
@@ -13,38 +13,57 @@ def index(request):
                 'jsonrpc': '2.0',
                 'id': req['id'],
             }
-            if request.META['HTTP_AUTHORIZATION'] == '12345':
-                match req['method']:
-                    case 'GetClientDetail':
-                        try:
-                            user = models.User.objects.get(barcode=req['params']['barcode'])
-                            data['result'] = {
-                                'name': user.name,
-                                'phone': user.phone,
-                                'barcode': user.barcode,
-                                'birth_date': user.birth_date,
-                                'city': user.city,
-                                'balance': user.balance
-                            }
-                            return JsonResponse(data=data)
-                        except ObjectDoesNotExist:
-                            data['error'] = ERRORS[1003]
-                            return JsonResponse(data)
-                    case 'NewPurchase':
-                        try:
-                            user = models.User.objects.get(barcode=req['params']['barcode'])
-
-                            data['result'] = 'Success'
-                            return JsonResponse(data)
-                        except ObjectDoesNotExist:
-                            data['error'] = ERRORS[1003]
-                            return JsonResponse(data)
-                    case _:
-                        data['error'] = ERRORS[1001]
-                        return JsonResponse(data)
-            else:
+            if request.META['HTTP_AUTHORIZATION'] != 'HTcA3EMBx9ViZIrimXhR2SgIHht4Hb':
                 data['error'] = ERRORS[1002]
                 return JsonResponse(data)
+            match req['method']:
+                case 'GetClientDetail':
+                    try:
+                        user = models.User.objects.get(barcode=req['params']['barcode'])
+                        data['result'] = {
+                            'name': user.name,
+                            'phone': user.phone,
+                            'barcode': user.barcode,
+                            'birth_date': user.birth_date,
+                            'city': user.city,
+                            'balance': user.balance
+                        }
+                        return JsonResponse(data=data)
+                    except ObjectDoesNotExist:
+                        data['error'] = ERRORS[1003]
+                        return JsonResponse(data)
+                case 'NewPurchase':
+                    try:
+                        types = ['cash', 'card', 'bonus']
+                        if req['params']['type'] not in types:
+                            data['error'] = ERRORS[1004]
+                            return JsonResponse(data)
+                        user = models.User.objects.get(barcode=req['params']['barcode'])
+                        if req['params']['type'] == 'bonus':
+                            if int(req['params']['total_sum'] / 100) > user.balance:
+                                data['error'] = ERRORS[1005]
+                                return JsonResponse(data)
+                            else:
+                                user.balance -= int(req['params']['total_sum'] / 100)
+                                user.save()
+                        else:
+                            user.balance += int(req['params']['total_sum'] / 10000)
+                            user.save()
+                        purchase = models.Purchase(
+                            user=user,
+                            sum=req['params']['total_sum'],
+                            type=req['params']['type'],
+                            datetime=req['params']['datetime'],
+                        )
+                        purchase.save()
+                        data['result'] = 'Success'
+                        return JsonResponse(data)
+                    except ObjectDoesNotExist:
+                        data['error'] = ERRORS[1003]
+                        return JsonResponse(data)
+                case _:
+                    data['error'] = ERRORS[1001]
+                    return JsonResponse(data)
 
         else:
             return HttpResponseBadRequest('Invalid JSON')
@@ -64,5 +83,13 @@ ERRORS = {
     1003: {
         'code': 1003,
         'message': 'Client does not exist'
+    },
+    1004: {
+        'code': 1004,
+        'message': 'Wrong payment type'
+    },
+    1005: {
+        'code': 1005,
+        'message': 'Not enough bonus'
     }
 }
