b439d5d4a7e954641be6d714ed1015dd29cad3da
AlexTheMaggot <alexthemaggot23@gmail.com>
2024-10-18 17:43:12 +0500

    - fix backupper

--- backupper.py
+++ backupper.py
@@ -1,12 +1,13 @@
 import os
 
 
-if 'backup.zip' in os.listdir('./'):
-    os.system('rm ./backup.zip')
+if 'backup.zip' in os.listdir('/home/lelit_loyalty/'):
+    os.system('rm /home/lelit_loyalty/backup.zip')
 
 
-os.system('mkdir backup')
-os.system('cp -r ./bot/barcodes ./backup/barcodes')
-os.system('cp ./lelit_loyalty/db.sqlite3 ./backup/db.sqlite3')
-os.system('zip -r backup.zip ./backup')
-os.system('rm -r ./backup')
+os.system('mkdir /home/lelit_loyalty/backup')
+os.system('cp -r /home/lelit_loyalty/bot/barcodes /home/lelit_loyalty/backup/barcodes')
+os.system('cp /home/lelit_loyalty/lelit_loyalty/db.sqlite3 /home/lelit_loyalty/backup/db.sqlite3')
+os.system('zip -r /home/lelit_loyalty/backup.zip /home/lelit_loyalty/backup')
+os.system('rm -r /home/lelit_loyalty/backup')
+print('Done!')
