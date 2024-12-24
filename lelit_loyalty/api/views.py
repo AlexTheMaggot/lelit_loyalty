8c818088b260d60579dd1ed0ca7d853d4aa515a3
AlexTheMaggot <alexthemaggot23@gmail.com>
2024-12-24 13:46:38 +0500

    - add payment notify

--- lelit_loyalty/api/views.py
+++ lelit_loyalty/api/views.py
@@ -3,7 +3,9 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.http import JsonResponse, HttpResponseBadRequest
 import json
 from . import models
-
+import datetime
+import requests
+from lelit_loyalty.config import *
 
 def index(request):
     try:
@@ -57,6 +59,20 @@ def index(request):
                         )
                         purchase.save()
                         data['result'] = 'Success'
+                        info_text = f'Новая оплата!\n\nБаркод: {user.barcode}\n'
+                        info_text += f'Сумма: {req["params"]["total_sum"] // 100} сум\n'
+                        if req['params']['type'] == 'cash':
+                            info_text += 'Тип оплаты: Наличные\n'
+                        elif req['params']['type'] == 'card':
+                            info_text += 'Тип оплаты: Карта\n'
+                        elif req['params']['type'] == 'bonus':
+                            info_text += 'Тип оплаты: Бонусы\n'
+                        info_datetime = datetime.datetime.fromtimestamp(req["params"]["datetime"])
+                        info_text += f'Дата платежа: {info_datetime.strftime('%d.%m.%Y %H:%M:%S')}'
+                        info_link = f'https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage'
+                        info_link += f'?chat_id=-1002432390765&text={info_text}'
+                        r = requests.get(url=info_link)
+                        print(r.text)
                         return JsonResponse(data)
                     except ObjectDoesNotExist:
                         data['error'] = ERRORS[1003]
